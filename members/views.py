480ed89974ce2486d5f83f911c647fcd6dc95137
Brian Lee <75905365+btlee264855@users.noreply.github.com>
2022-11-11 22:47:24 -0500

    adding members

--- members/views.py
+++ members/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Members
 
-# Create your views here.
+def index(request):
+  mymembers = Members.objects.all().values()
+  output = ""
+  for x in mymembers:
+    output += x["firstname"]
+  return HttpResponse(output)
