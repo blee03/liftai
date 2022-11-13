3bba373c2558fbf051e87bbe7cce2f11fcff52d3
PrestonXPitzer <prestonpitzer20@vt.edu>
2022-11-13 03:17:33 -0500

    i fixed the dict

--- members/videosdict.py
+++ members/videosdict.py
@@ -1,24 +1,36 @@
 #place to pull the video URLs from
 
 #the array containing the coefficents of how many of each excercise to do for arms
-Aexcer = np.array([https://www.youtube.com/embed/vK0haV9km24,
-https://www.youtube.com/embed/tkp22GWXAMI,
-https://www.youtube.com/embed/ERXQ3TFPBmI,
-https://www.youtube.com/embed/PEbucj4fqhE,
-https://www.youtube.com/embed/0irSr7p0JdY,uexcer6,
-uexcer7,uexcer8,uexcer9,uexcer10,uexcer11,uexcer12,
-https://www.youtube.com/embed/fO82H3F0rw4,
-https://www.youtube.com/embed/mEbC-y4hs9Q
-,uexcer15,uexcer16 ])
+Aexcer =("https://www.youtube.com/embed/vK0haV9km24",
+"https://www.youtube.com/embed/tkp22GWXAMI",
+"https://www.youtube.com/embed/ERXQ3TFPBmI",
+"https://www.youtube.com/embed/PEbucj4fqhE",
+"https://www.youtube.com/embed/0irSr7p0JdY",
+"https://www.youtube.com/embed/CJxH6e2R1Ow",
+"https://www.youtube.com/embed/i9OsIUNWAOU",
+"https://www.youtube.com/embed/MZFvO5VivzQ",
+"https://www.youtube.com/embed/CJxH6e2R1Ow",
+"https://www.youtube.com/embed/XVP0EPpKzlk",
+"https://www.youtube.com/embed/CJxH6e2R1Ow",
+"https://www.youtube.com/embed/dt0CYXuZTq8",
+"https://www.youtube.com/embed/fO82H3F0rw4",
+"https://www.youtube.com/embed/mEbC-y4hs9Q",
+"https://www.youtube.com/embed/oeJeIQjX7Mw",
+"https://www.youtube.com/embed/3_E8lkDdQJs")
 #the array containing the coefficents of how many of each excercise to do for legs
-Lexcer = np.array([https://www.youtube.com/embed/siIGhA0r4us,
-https://www.youtube.com/embed/9GxQ96tlZns,
-https://www.youtube.com/embed/ySAA8Ja01hM,
-https://www.youtube.com/embed/WG_g-X-ss98,
-lexcer5,
-https://www.youtube.com/embed/Lz1Oy2i6-Go,
-lexcer7,lexcer8,lexcer9,
-https://www.youtube.com/embed/nvwT_X5C_38,
-https://www.youtube.com/embed/WNfmSUKg2Rs
-,lexcer12,lexcer13,lexcer14,lexcer15,
-https://www.youtube.com/embed/thGKGVXQdUE ])
+Lexcer =("https://www.youtube.com/embed/siIGhA0r4us",
+"https://www.youtube.com/embed/9GxQ96tlZns",
+"https://www.youtube.com/embed/ySAA8Ja01hM",
+"https://www.youtube.com/embed/WG_g-X-ss98",
+"https://www.youtube.com/embed/fDJLc2zepNM",#adductor
+"https://www.youtube.com/embed/Lz1Oy2i6-Go",
+"https://www.youtube.com/embed/YhDRqWG47qA", #calf
+"https://www.youtube.com/embed/6l32mMzVrFg", #leg extension
+"https://www.youtube.com/embed/YhDRqWG47qA", #calf machine
+"https://www.youtube.com/embed/nvwT_X5C_38",
+"https://www.youtube.com/embed/WNfmSUKg2Rs",
+"https://www.youtube.com/embed/fDJLc2zepNM", #band
+"https://www.youtube.com/embed/fDJLc2zepNM", #seated band
+"https://www.youtube.com/embed/H_nYiD_PYfA", #romaninan
+"https://www.youtube.com/embed/zQ-tYsCeuPk", #hip thrust
+"https://www.youtube.com/embed/thGKGVXQdUE")
